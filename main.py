b46dcbb96f1e1298defa3f5ef20a7b55d1ed7f45
martabritala <53839047+martabritala@users.noreply.github.com>
2020-11-12 13:36:08 +0200

    elementi

--- main.py
+++ main.py
@@ -13,39 +13,43 @@ app = Flask('app')
 #         self.izvele=izvele
 #         self.teksts=teksts
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index_lapa():
-    return render_template('index.html')
-
-@app.route('/gatavs', methods=['POST', 'GET'])
-def gatavs():
     if request.method=='POST':
-        stundas = request.form.getlist('stunduskaits[]')
-        print(stundas[0])
-        stundas = stundas[0].split(',')
-        # for elem in stundas:
-        #     print(elem)
-        #     elem=int(elem)
-        print(stundas)
-        print(request.form.getlist('izvele[]'))
-        print(request.form)
-        return render_template('gatavs.html',numurs = request.form['ExNr'], klase10=int(stundas[0]), klase11=int(stundas[1]), klase12=int(stundas[2]), tresa=request.form['tresaval'], otra=request.form['otraval'], vards=request.form['vards'], uzvards=request.form['uzvards'], programma=request.form['programma'], masivs=request.form.getlist('izvele[]'))
+        elementi='Nosutijas'
     else:
-        return render_template('gatavs.html', klase10=32, klase11=30, klase12=18, vards='Jānis', uzvards='Bērziņš', programma=1, masivs=[30,18,13])
+        elementi=''
+    return render_template('index.html',teksts=elementi)
+
+# @app.route('/gatavs', methods=['POST', 'GET'])
+# def gatavs():
+#     if request.method=='POST':
+#         stundas = request.form.getlist('stunduskaits[]')
+#         print(stundas[0])
+#         stundas = stundas[0].split(',')
+#         # for elem in stundas:
+#         #     print(elem)
+#         #     elem=int(elem)
+#         print(stundas)
+#         print(request.form.getlist('izvele[]'))
+#         print(request.form)
+#         return render_template('gatavs.html',numurs = request.form['ExNr'], klase10=int(stundas[0]), klase11=int(stundas[1]), klase12=int(stundas[2]), tresa=request.form['tresaval'], otra=request.form['otraval'], vards=request.form['vards'], uzvards=request.form['uzvards'], programma=request.form['programma'], masivs=request.form.getlist('izvele[]'))
+#     else:
+#         return render_template('gatavs.html', klase10=32, klase11=30, klase12=18, vards='Jānis', uzvards='Bērziņš', programma=1, masivs=[30,18,13])
 
 @app.route('/test')
 def test():
     return data.test_connection()
 
-@app.route('/beigas')
-def beigas():
-    return redirect('https://www.rag.lv/')
+# @app.route('/beigas')
+# def beigas():
+#     return redirect('https://www.rag.lv/')
 
-@app.route('/suutiit', methods=['POST'])
-def suutiit():
-    parametri="'{vards}','{uzvards}',{programma},{datori},{debates},{anglit},{filoz},{publ},{papang},{psih},{robo},{krv},{padz1},{padz2},{padz3},{otraval},{tresaval},{exnumurs}".format(**request.json)
-    data.ierakstit(parametri)
-    return data.test_connection()
+# @app.route('/suutiit', methods=['POST'])
+# def suutiit():
+#     parametri="'{vards}','{uzvards}',{programma},{datori},{debates},{anglit},{filoz},{publ},{papang},{psih},{robo},{krv},{padz1},{padz2},{padz3},{otraval},{tresaval},{exnumurs}".format(**request.json)
+#     data.ierakstit(parametri)
+#     return data.test_connection()
 
 @app.route('/mekletajs', methods=['POST', 'GET'])
 def meklet():
