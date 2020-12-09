85abc22cc46da69f51a3b4db5c5c3ec27d4b3e43
martabritala <53839047+martabritala@users.noreply.github.com>
2020-12-09 22:28:44 +0200

    test?

--- static/tests5.js
+++ static/tests5.js
@@ -1,8 +1,8 @@
 function ieladejotPievienot(saites){
-    for(let objekts=0;objekts<7;objekts++){
+    for(let objekts=1;objekts<8;objekts++){
         let lieta = document.getElementById('img'+objekts)
         console.log(objekts, "bus")
-        lieta.addEventListener('click',function = (eve) =>{
+        lieta.addEventListener('click',function(eve) {
             inforadit(objekts);
         })
     }
