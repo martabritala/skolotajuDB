a1ab9ecf2444b9ad8955836e652f95a5fe7c5184
martabritala <53839047+martabritala@users.noreply.github.com>
2020-12-10 04:22:47 +0200

    otra kolonna??

--- static/tests5.js
+++ static/tests5.js
@@ -9,7 +9,14 @@ function ieladejotPievienot(saites){
     console.log(saites)
 }
 
-const tabula = ["<h4>Maksa</h4></br><ul><li>Bezmaksas - viss saturs ir par brīvu</li><li>Daļēji maksas - pamatsaturs ir par brīvu, bet daļa satura/funkcionalitātes - par maksu</li></ul>","divi","tris","cetri","pieci","sesi","septini","astoni"];
+const tabula = ["<h4>Maksa</h4></br><ul><li>Bezmaksas - viss saturs ir par brīvu</li><li>Daļēji maksas - pamatsaturs ir par brīvu, bet daļa satura/funkcionalitātes - par maksu</li></ul>",
+... "<h4>Kurš veido/lieto?</h4></br><ul><li>Lieto gatavu IT resursu - nodarbībā to izmanto</li><li>Veido IT resursu - nodarbībā to veido</li><li>Mācību darbā/-am SK - resurss skolotājam</li><li>Prof. pilnveide SK - resurss skolotājam</li><li>Pirmsskolas SN - resurss  pirmskolas posma skolēnam</li><li>Sākumskolas SN - resurss sākumizglītības posma skolēnam</li><li>Pamatskolas SN - resurss pamatizglītības posma skolēnam</li><li>Vidusskola I SN - resurss vidusskolas pamatkursa skolēnam</li><li>Vidusskola II SN - resurss vidusskolas padziļinātā kursa skolēniem</li><li>Augstskola - resurss studentam</li></ul>",
+..."tris",
+..."cetri",
+..."pieci",
+..."sesi",
+..."septini",
+..."astoni"];
 
 function inforadit(id){
     console.log(id);
