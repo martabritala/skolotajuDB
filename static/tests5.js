8717769b33c3237dc69954a3d9e11a3b74a98608
martabritala <53839047+martabritala@users.noreply.github.com>
2020-12-10 04:54:31 +0200

    ???

--- static/tests5.js
+++ static/tests5.js
@@ -6,13 +6,13 @@ function ieladejotPievienot(saites){
             inforadit(objekts);
         })
     }
-    console.log(saites)
+    console.log(saites[0], saites[1],saites[2])
 }
 
 const tabula = ["<h4>Maksa</h4></br><ul><li>Bezmaksas - viss saturs ir par brīvu</li><li>Daļēji maksas - pamatsaturs ir par brīvu, bet daļa satura/funkcionalitātes - par maksu</li></ul>",
  "<h4>Kurš veido/lieto?</h4></br><ul><li>Lieto gatavu IT resursu - nodarbībā to izmanto</li><li>Veido IT resursu - nodarbībā to veido</li><li>Mācību darbā/-am SK - resurss skolotājam</li><li>Prof. pilnveide SK - resurss skolotājam</li><li>Pirmsskolas SN - resurss  pirmskolas posma skolēnam</li><li>Sākumskolas SN - resurss sākumizglītības posma skolēnam</li><li>Pamatskolas SN - resurss pamatizglītības posma skolēnam</li><li>Vidusskola I SN - resurss vidusskolas pamatkursa skolēnam</li><li>Vidusskola II SN - resurss vidusskolas padziļinātā kursa skolēniem</li><li>Augstskola - resurss studentam</li></ul>",
 "<h4>Saturs</h4></br><ul><li>Konkrēts mācību kurss <i>(Ķīmija, Bioloģija, Fizika, Ģeogrāfija, Tehnoloģijas, Matemātika, IT)</i></li><li>Dabaszinības - integrētais dabaszinātņu kurss</li><li> Dabaszinātnes - vispārīgs resurss, noderīgs daudzos mācību kursos</li><li>Mācību progr. - dota un analizēta mācību programma vai to modulis</li><li>Nodarbību plāns - dots nodarbības plāns</li><li>Metodiskais materiāls - analizēta kādas mācību stratēģijas (metodikas) izmantošana</li></ul>",
-"cetri",
+"<h4>Resursa veids<h4></br><ul><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul>",
 "pieci",
 "sesi",
 "septini",
